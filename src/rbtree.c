ec0af8ff25dbb93205481e562e78232b70530a55
LemonEaterMJ <98jjyoung@gmail.com>
2022-11-29 00:06:46 +0900

    find_erase_fixed 까지 통과

--- src/rbtree.c
+++ src/rbtree.c
@@ -294,22 +294,20 @@ void rbtree_insert_fixup(const rbtree *t, node_t *z) {
 node_t *rbtree_find(const rbtree *t, const key_t key) {
 	if (!t || !(t->root)) {	//tree 구성 전
 		return NULL;
-	} 
-
-	node_t *temp = t->root;
-	while(temp != t->nil) {
-		if (key == t->root->key) {//찾았다!
-			return temp;
-		} else if (key < temp->key) {	//left branch로 진행
-			temp = temp->left;
-		} else {	// right branch 로 진행
-			temp = temp->right;
-		}
-	} 
-	// 끝까지 찾았는데 없었다! == temp = t->nil
-	return NULL;
-	
-    
+	} else {
+		node_t *temp = t->root;
+		while(temp != t->nil) {
+			if (key == temp->key) {//찾았다!
+				return temp;
+			} else if (key < temp->key) {	//left branch로 진행
+				temp = temp->left;
+			} else {	// right branch 로 진행
+				temp = temp->right;
+			}
+		} 
+		// 끝까지 찾았는데 없었다! ==> temp == t->nil
+		return NULL;
+	}
 }
 
 
